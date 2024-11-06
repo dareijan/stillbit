f12cbb79549a5ba77abc58343349ff68662ee140
dareijan <tarpamama@protonmail.com>
2024-11-06 23:52:30 +0200

    Käyttöliittymäpäivitys, mukaan päivämäärät vuorokauden vaihtuessa

--- src/App.js
+++ src/App.js
@@ -17,15 +17,20 @@ const apibase =   'https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request
 
 export default function App() {  
 
-  const [säädata_1, setTuntiData1] = useState([]);
-  const [säädata_2, setTuntiData2] = useState([]);
-  const [säädata_3, setTuntiData3] = useState([]);
+  
+  const [saadata_1, setTuntiData1] = useState([]);
+  const [saadata_2, setTuntiData2] = useState([]);
+  const [saadata_3, setTuntiData3] = useState([]);
   const [paivays, setPaivaysData] = useState([]);
 
   const [state, setState] = useState({
     data: [],
     limit: 5,
-    activePage: 1
+    activePage: 1,
+    saadata1: [],
+    saadata2: [],
+    saadata3: []
+    //jostain syystä funktion sisällä nämä näkyy sitten katoaa kun piirtäminen alkaa
   });  
 
     useEffect(() => {
@@ -37,106 +42,132 @@ export default function App() {
       const paiva = String(paivamaara.getDate()).padStart(2, '0');
       const tunti = String(paivamaara.getHours()).padStart(2, '0');
       const paivays = vuosi + "-" + kuukausi + "-" + paiva + "T" + tunti + ":00:00Z"
-      axios
-      .get((apibase+"&starttime="+paivays)).then((result) => {
 
-        var poissuljettavat_yön_ajat = ["01:00", "02:00", "03:00", "04:00", "05:00", "06:00"];
+      //useita axios requesteja tapahtuu, joten estetään se
+      if(state.data.length <= 0) {
+        axios
+        .get((apibase+"&starttime="+paivays)).then((result) => {
 
-        let parser = new DOMParser();
-        let parsedxml = parser.parseFromString(result.data, "text/xml");
-        var lkm=0;
-        parsedxml.querySelectorAll('MeasurementTVP').forEach((measurementTVPObjektit) => {
-            let value = measurementTVPObjektit.querySelector('value').textContent;
-            let time = measurementTVPObjektit.querySelector('time').textContent;
+          var poissuljettavat_yön_ajat = ["01:00", "02:00", "03:00", "04:00", "05:00", "06:00"];
 
-            var kokonainenaika = time.split(/T/);
-            var kellonaika = kokonainenaika[1].split(/:/);
-            var kellonaika0 = kellonaika[0];
-            var kellonaika1 = kellonaika[1];
-            var uusiaika = kellonaika0 + ":" + kellonaika1;
-         //   var uusiaika = kokonainenaika;
-            var ensimmainenaamu = false;
-            if (poissuljettavat_yön_ajat.indexOf(uusiaika) < 0) {  
-              lkm++;
-              if (lkm < 20) {
-                säädata_1.push({uusiaika, value});
-              } else if (lkm >= 20 && lkm < 50) {
-                säädata_2.push({uusiaika, value});
-              } else  if (lkm >= 50){
-                säädata_3.push({uusiaika, value});
-              }            
-             }
-         });
-         setState({data: säädata_1});
-         //setState((prev) => ({
-         // ...prev,
-         // data: säädata_1
-        //}));                    
-      })
-      .catch((error) => console.log(error));
-      }, [state.limit]);
+          let parser = new DOMParser();
+          let parsedxml = parser.parseFromString(result.data, "text/xml");
+          var lkm=0;
+          parsedxml.querySelectorAll('MeasurementTVP').forEach((measurementTVPObjektit) => {
+
+              let value = measurementTVPObjektit.querySelector('value').textContent;
+              let time = measurementTVPObjektit.querySelector('time').textContent;
+
+              var kokonainenaika = time.split(/T/);
+              var kellonaika = kokonainenaika[1].split(/:/);
+              var kellonaika0 = kellonaika[0];
+              var kellonaika1 = kellonaika[1];
+              var uusiaika = kellonaika0 + ":" + kellonaika1;
+
+              var huomenna = 0;
+              var paivamaarateksti = '';
+              if(state.saadata1.length <= 25) {
+
+                if (poissuljettavat_yön_ajat.indexOf(uusiaika) < 0) {  
+                  lkm++;
+                  //päätellään milloin pitää piirtää huomisteksti
+                  if(uusiaika == '07:00' && huomenna == 0) {
+                    paivamaarateksti = kokonainenaika[0];
+                    huomenna = 1;
+                  }
+                  if (lkm <= 25) {
+                    setState({saadata1: state.saadata1.push({uusiaika, paivamaarateksti, value})});
+                    setState({data: state.data.push({uusiaika, paivamaarateksti, value})});              
+                  } else if (lkm > 25 && lkm <= 50) {
+                    setState({saadata2: state.saadata2.push({uusiaika, paivamaarateksti, value})});
+                  } else  if (lkm > 50){
+                    setState({saadata3: state.saadata3.push({uusiaika, paivamaarateksti, value})});      
+                  }            
+                }
+                paivamaarateksti = '';
+            }
+          });
+          // ei näin setState({data: state.saadata_1});
+          setState((prev) => ({
+            ...prev,
+            data: state.saadata1
+          }));
+
+         setTuntiData1(state.saadata1);
+         setTuntiData2(state.saadata2);
+         setTuntiData3(state.saadata3);
+                 
+        })
+        .catch((error) => console.log(error));
+    }
+      }, [state.data]);
 
       const handlePageChange = (pageNumber) => {
         if (pageNumber == 1) {
           setState((prev) => ({
             ...prev,
-            data: säädata_1
+            data: saadata_1
           }));
         state.activePage = 1;
         } else if (pageNumber == 2) {
           setState((prev) => ({
             ...prev,
-            data: säädata_2
+            data: saadata_2
           }));
         state.activePage = 2;
         } else {
           setState((prev) => ({
             ...prev,
-            data: säädata_3
+            data: saadata_3
           }));
           state.activePage = 3;
         }
       };
-//&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    //&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
     return (
       <div>
+      <div class="p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3" align="center">
+        <b>Stillbits </b>kertoo sijainnin äänekkyyden tuulen suuntaan mukaan tunneittain  <br />... vuorokausi vaihtuu sinisestä viivasta ... eikä yön tunteja yhdestä seitsemään huomioida  <br /> <br />
 
-      <Container>
-      <Row>
-      <Col md="9">
-      
-        <Pagination className="justify-content-center">
-              <Pagination.Item
-                onClick={() => handlePageChange(1)}
-                key={1}
-                active={1 === state.activePage}
-              >
-              {1}
-              </Pagination.Item>
+        <Container>
+        <Row>
+        <Col md="9">
+        
+          <Pagination className="justify-content-center">
+                <Pagination.Item
+                  onClick={() => handlePageChange(1)}
+                  key={1}
+                  active={1 === state.activePage}
+                >
+                {1}
+                </Pagination.Item>
 
-              <Pagination.Item
-                onClick={() => handlePageChange(2)}
-                key={2}
-                active={2 === state.activePage}
-              >
-              {2}
-              </Pagination.Item>
+                <Pagination.Item
+                  onClick={() => handlePageChange(2)}
+                  key={2}
+                  active={2 === state.activePage}
+                >
+                {2}
+                </Pagination.Item>
 
-              <Pagination.Item
-                onClick={() => handlePageChange(3)}
-                key={3}
-                active={3 === state.activePage}
-              >
-              {3}
-              </Pagination.Item>
+                <Pagination.Item
+                  onClick={() => handlePageChange(3)}
+                  key={3}
+                  active={3 === state.activePage}
+                >
+                {3}
+                </Pagination.Item>
 
-        </Pagination>
-        </Col>
-      </Row>
-      </Container>
-      <TuuliKomponentti data={state.data} />
-      
+          </Pagination>
+          </Col>
+        </Row>
+        </Container>
       </div>
 
+      <TuuliKomponentti data={state.data} />
+      <div class="p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3" align="center">
+      <br /> <br />
+      </div>     
+      </div>
     );
   };
--- src/komponentit/TuuliKomponentti.js
+++ src/komponentit/TuuliKomponentti.js
@@ -6,7 +6,6 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 
 const TuuliKomponentti = ({ data }) => {
     const rows = data.map(item => Object.values(item));
-    var tehdaanviiva = false;
 
     //    let taulukko =  Object.values({data}); //= Array.from(data);
 
@@ -16,28 +15,32 @@ const TuuliKomponentti = ({ data }) => {
     // Row  className="row jsustify-content-center"
 
     return (
+      <span class="border border-primary">
       <Container>
+
           {rows.map((row, index1) => (
             <Row>
                 {row.map((cell, index) => 
                   <Col md="9">
                     <span align="center">
 
-                      <h4>
-                      {cell}
-                      {cell=='07:00'? <hr class="hr-text-aamu" data-content="07:00"></hr> : ""}
 
+
+                      {cell=='07:00'? <hr class="hr-text-aamu" data-content="07:00" data-bs-theme="blue"></hr> : ""}
+
+                      {index==1? cell :"" }
+                      <h2>
                       {cell=='12:00'? <hr class="hr-text" data-content="12:00"></hr> : ""}
                 
                       {cell=='18:00'? <hr class="hr-text" data-content="18:00"></hr> : ""}
 
-                      {index==1 && (cell>=90 && cell<=270 ) ? <i class="bi bi-cloud-fog2-fill harmaa" title={cell}></i>:""}
+                      {index==2 && (cell>=90 && cell<=270 ) ? <i class="bi bi-cloud-fog2-fill harmaa" title={cell}></i>:""}
 
-                      {index==1 && ((cell<120 && cell>90) || (cell>270 && cell<300)) ? <i class="bi bi-cloud-haze harmaa" title={cell}></i>:""}
+                      {index==2 && ((cell<120 && cell>90) || (cell>270 && cell<300)) ? <i class="bi bi-cloud-haze harmaa" title={cell}></i>:""}
 
-                      {index==1 && (cell<90 || cell>300 ) ? <i class="bi bi-cloud-check harmaa" title={cell}></i>:""}    
+                      {index==2 && (cell<90 || cell>300 ) ? <i class="bi bi-cloud-check harmaa" title={cell}></i>:""}    
 
-                    </h4>
+                    </h2>
                     </span>
 
                   </Col>
@@ -45,6 +48,8 @@ const TuuliKomponentti = ({ data }) => {
           </Row>
           ))}
       </Container>
+      </span>
+
     );
   };
   
